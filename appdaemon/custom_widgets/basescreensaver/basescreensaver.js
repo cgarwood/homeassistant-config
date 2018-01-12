0e44e60d10da393545c942e57d5d80b68846413b
Charles Garwood <cgarwood@gmail.com>
2018-01-12 17:42:02 -0500

    Weather slide cleanup

--- appdaemon/custom_widgets/basescreensaver/basescreensaver.js
+++ appdaemon/custom_widgets/basescreensaver/basescreensaver.js
@@ -36,88 +36,47 @@ function basescreensaver(widget_id, url, skin, parameters) {
 		{"entity": "sensor.pws_alerts", "initial": self.OnStateAvailable, "update": self.OnStateUpdate}
 	];
 	
-	//Mapping Wunderground icons to background images
-	//TODO: Combine self.bg_mapping and self.weather_icons into a single object
-	self.bg_mapping = {
-		'chanceflurries' : 'snow.jpg',
-		'chancerain' : 'rain.jpg',
-		'chancesleet' : 'rain.jpg',
-		'chancesnow' : 'snow.jpg',
-		'chancetstorms' : 'thunderstorm.jpg',
-		'clear' : 'sunny.jpg',
-		'cloudy' : 'cloudy.jpg',
-		'flurries' : 'snow.jpg',
-		'fog' : 'fog.jpg',
-		'hazy' : 'fog.jpg',
-		'mostlycloudy' : 'partlycloudy.jpg',
-		'mostlysunny' : 'partlycloudy.jpg',
-		'partlycloudy' : 'partlycloudy.jpg',
-		'partlysunny' : 'partlycloudy.jpg',
-		'sleet' : 'rain.jpg',
-		'rain' : 'rain.jpg',
-		'snow' : 'snow.jpg',
-		'sunny' : 'sunny.jpg',
-		'tstorms' : 'thunderstorm.jpg',
-		'nt_chanceflurries' : 'nt_snow.jpg',
-		'nt_chancerain' : 'nt_rain.jpg',
-		'nt_chancesleet' : 'nt_rain.jpg',
-		'nt_chancesnow' : 'nt_snow.jpg',
-		'nt_chancetstorms' : 'nt_thunderstorm.jpg',
-		'nt_clear' : 'nt_sunny.jpg',
-		'nt_cloudy' : 'nt_cloudy.jpg',
-		'nt_flurries' : 'nt_snow.jpg',
-		'nt_fog' : 'nt_fog.jpg',
-		'nt_hazy' : 'nt_fog.jpg',
-		'nt_mostlycloudy' : 'nt_cloudy.jpg',
-		'nt_mostlysunny' : 'nt_sunny.jpg',
-		'nt_partlycloudy' : 'nt_cloudy.jpg',
-		'nt_partlysunny' : 'nt_cloudy.jpg',
-		'nt_sleet' : 'nt_rain.jpg',
-		'nt_rain' : 'nt_rain.jpg',
-		'nt_snow' : 'nt_snow.jpg',
-		'nt_sunny' : 'nt_sunny.jpg',
-		'nt_tstorms' : 'nt_thunderstorm.jpg'
+	//Mapping Wunderground icons to new icons and background images
+	self.weather_mapping = {
+		'chanceflurries' : {'icon':'&#xe036','bg':'snow.jpg'},
+		'chancerain' : {'icon':'&#xe009', 'bg':'rain.jpg'},
+		'chancesleet' : {'icon':'&#xe009g','bg':'rain.jpg'},
+		'chancesnow' : {'icon':'&#xe036','bg':'snow.jpg'},
+		'chancetstorms' : {'icon':'&#xe026','bg':'thunderstorm.jpg'},
+		'clear' : {'icon':'&#xe028','bg':'sunny.jpg'},
+		'cloudy' : {'icon':'&#xe000','bg':'cloudy.jpg'},
+		'flurries' : {'icon':'&#xe036','bg':'snow.jpg'},
+		'fog' : {'icon':'&#xe01b','bg':'fog.jpg'},
+		'hazy' : {'icon':'&#xe01b','bg':'fog.jpg'},
+		'mostlycloudy' : {'icon':'&#xe001','bg':'partlycloudy.jpg'},
+		'mostlysunny' : {'icon':'&#xe001','bg':'sunny.jpg'},
+		'partlycloudy' : {'icon':'&#xe001','bg':'partlycloudy.jpg'},
+		'partlysunny' : {'icon':'&#xe001','bg':'partlycloudy.jpg'},
+		'sleet' : {'icon':'&#xe009','bg':'rain.jpg'},
+		'rain' : {'icon':'&#xe009','bg':'rain.jpg'},
+		'snow' : {'icon':'&#xe036','bg':'snow.jpg'},
+		'sunny' : {'icon':'&#xe028','bg':'sunny.jpg'},
+		'tstorms' : {'icon':'&#xe025','bg':'thunderstorms.jpg'},
+		'nt_chanceflurries' : {'icon':'&#xe036','bg':'nt_snow.jpg'},
+		'nt_chancerain' : {'icon':'&#xe009','bg':'nt_rain.jpg'},
+		'nt_chancesleet' : {'icon':'&#xe009','bg':'nt_rain.jpg'},
+		'nt_chancesnow' : {'icon':'&#xe036','bg':'nt_snow.jpg'},
+		'nt_chancetstorms' : {'icon':'&#xe027','bg':'nt_thunderstorm.jpg'},
+		'nt_clear' : {'icon':'&#xe02d','bg':'nt_sunny.jpg'},
+		'nt_cloudy' : {'icon':'&#xe000','bg':'nt_cloudy.jpg'},
+		'nt_flurries' : {'icon':'&#xe036','bg':'nt_snow.jpg'},
+		'nt_fog' : {'icon':'&#xe01b','bg':'nt_fog.jpg'},
+		'nt_hazy' : {'icon':'&#xe01b','bg':'nt_fog.jpg'},
+		'nt_mostlycloudy' : {'icon':'&#xe000','bg':'nt_cloudy.jpg'},
+		'nt_mostlysunny' : {'icon':'&#xe02d','bg':'nt_sunny.jpg'},
+		'nt_partlycloudy' : {'icon':'&#xe002','bg':'nt_cloudy.jpg'},
+		'nt_partlysunny' : {'icon':'&#xe000','bg':'nt_cloudy.jpg'},
+		'nt_sleet' : {'icon':'&#xe003','bg':'nt_rain.jpg'},
+		'nt_rain' : {'icon':'&#xe009','bg':'nt_rain.jpg'},
+		'nt_snow' : {'icon':'&#xe036','bg':'nt_snow.jpg'},
+		'nt_sunny' : {'icon':'&#xe02d','bg':'nt_sunny.jpg'},
+		'nt_tstorms' : {'icon':'&#xe025','bg':'nt_thunderstorm.jpg'}
 	}
-	self.weather_icons = {
-		'chanceflurries' : '&#xe036',
-		'chancerain' : '&#xe009',
-		'chancesleet' : '&#xe009g',
-		'chancesnow' : '&#xe036',
-		'chancetstorms' : '&#xe026',
-		'clear' : '&#xe028',
-		'cloudy' : '&#xe000',
-		'flurries' : '&#xe036',
-		'fog' : '&#xe01b',
-		'hazy' : '&#xe01b',
-		'mostlycloudy' : '&#xe001',
-		'mostlysunny' : '&#xe001',
-		'partlycloudy' : '&#xe001',
-		'partlysunny' : '&#xe001',
-		'sleet' : '&#xe009',
-		'rain' : '&#xe009',
-		'snow' : '&#xe036',
-		'sunny' : '&#xe028',
-		'tstorms' : '&#xe025',
-		'nt_chanceflurries' : '&#xe036',
-		'nt_chancerain' : '&#xe009',
-		'nt_chancesleet' : '&#xe009',
-		'nt_chancesnow' : '&#xe036',
-		'nt_chancetstorms' : '&#xe027',
-		'nt_clear' : '&#xe02d',
-		'nt_cloudy' : '&#xe000',
-		'nt_flurries' : '&#xe036',
-		'nt_fog' : '&#xe01b',
-		'nt_hazy' : '&#xe01b',
-		'nt_mostlycloudy' : '&#xe000',
-		'nt_mostlysunny' : '&#xe02d',
-		'nt_partlycloudy' : '&#xe002',
-		'nt_partlysunny' : '&#xe000',
-		'nt_sleet' : '&#xe003',
-		'nt_rain' : '&#xe009',
-		'nt_snow' : '&#xe036',
-		'nt_sunny' : '&#xe02d',
-		'nt_tstorms' : '&#xe025'
-	};
 	
 	self.weather_alert_count = 0;
 	
@@ -259,13 +218,10 @@ function basescreensaver(widget_id, url, skin, parameters) {
 	function OnStateUpdate(self, state) {
 		if (state.entity_id == "sensor.pws_alerts") {
 			if (parseInt(state.state) > 0) {
-				//create the alerts slide if it doesnt exist
 				self.set_field(self, "weather_alert_title", state.attributes.Description)
 				self.set_field(self, "weather_alert_message", state.attributes.Messagereplace(/([^>\r\n\r\n]?)(\r\n\r\n|\n\r\n\r|\r\r|\n\n)/g, '$1' + '<br><br>' + '$2'))
 				self.set_field(self, "weather_alert_expires", state.attributes.Expires)
 				self.set_field(self, "weather_alert_date", state.attributes.Date)
-			} else {
-				//remove the alerts slide if it exists
 			}
 			self.weather_alert_count = state.state;
 		}
@@ -297,19 +253,19 @@ function basescreensaver(widget_id, url, skin, parameters) {
 			
 			var string = state.attributes.entity_picture.split('/');
 			var icon = string[string.length-1].split('.');
-			if (self.bg_mapping[icon[0]] !== undefined) {
+			if (self.weather_mapping[icon[0]]['icon'] !== undefined) {
 				if (!isNaN(day)) {
-					self.set_field(self, "weather_icon_"+day+"d", self.weather_icons[icon[0]])
+					self.set_field(self, "weather_icon_"+day+"d", self.weather_mapping[icon[0]]['icon'])
 				} else {
-					self.set_field(self, "weather_icon", self.weather_icons[icon[0]])
+					self.set_field(self, "weather_icon", self.weather_mapping[icon[0]]['icon'])
 				}
 			}
 		}
 		if (state.entity_id == "sensor.pws_weather") {
 			var string = state.attributes.entity_picture.split('/');
 			var icon = string[string.length-1].split('.');
-			if (self.bg_mapping[icon[0]] !== undefined) {
-				var background = self.bg_mapping[icon[0]];
+			if (self.weather_mapping[icon[0]]['bg'] !== undefined) {
+				var background = self.weather_mapping[icon[0]]['bg'];
 			} else {
 				//Default background
 				var background = 'partlycloudy.jpg';
