5b0753b97b102a9b1ec5a3da784aebb594db5be3
Charles Garwood <cgarwood@gmail.com>
2018-05-07 07:28:34 -0400

    fan remote updates

--- appdaemon/apps/fanremotes.py
+++ appdaemon/apps/fanremotes.py
@@ -8,19 +8,23 @@ class FanRemotes(hass.Hass):
 
     def initialize(self):
         self.listen_state(self.handle_light, "sensor.office_light_pico")
+        self.listen_state(self.handle_light, "sensor.master_bedroom_light_pico")
         self.listen_state(self.handle_fan, "sensor.office_fan_pico")
+        self.listen_state(self.handle_fan, "sensor.master_bedroom_fan_pico")
         
     def handle_light(self, entity, attribute, old, new, kwargs):
         if (entity == "sensor.office_light_pico"):
             controlled_entity = "light.office"
+        if (entity == "sensor.master_bedroom_light_pico"):
+            controlled_entity = "light.master_bedroom"
             
         if (new == '1'):
             # On
-            self.call_service('homeassistant/turn_on', entity_id = controlled_entity)
+            self.call_service('homeassistant/turn_on', entity_id = controlled_entity, brightness = 255)
             
         if (new == '2'):
             # Favorite
-            pass
+            self.call_service('homeassistant/turn_on', entity_id = controlled_entity, brightness = 25)
             
         if (new == '4'):
             # Off
@@ -38,6 +42,8 @@ class FanRemotes(hass.Hass):
         
         if (entity == "sensor.office_fan_pico"):
             controlled_entity = "fan.office_fan"
+        if (entity == "sensor.master_bedroom_fan_pico"):
+            controlled_entity = "fan.master_bedroom_fan"
             
         if (new == '1'):
             # On
