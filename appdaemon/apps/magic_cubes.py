d37b0f27753f8b3853127175969e2c7275618ee4
Charles Garwood <cgarwood@gmail.com>
2019-01-01 15:05:54 -0500

    formatting

--- appdaemon/apps/magic_cubes.py
+++ appdaemon/apps/magic_cubes.py
@@ -4,6 +4,7 @@
 
 import appdaemon.plugins.hass.hassapi as hass
 
+
 class MagicCubes(hass.Hass):
     def initialize(self):
         self.listen_event(self.handle_cube, 'zha_event')
@@ -17,11 +18,13 @@ class MagicCubes(hass.Hass):
         print(data)
         if (data['unique_id'] == 'zha_event.zha_029bd185_2'):
             if (data['command'] == 'flip'):
-                self.call_service('homeassistant/toggle', entity_id = 'group.christmas')
+                self.call_service('homeassistant/toggle',
+                                  entity_id='group.christmas')
 
         if (data['unique_id'] == 'zha_event.zha_029bd185_3'):
             if (data['command'] == 'rotate_left'):
-                self.call_service('homeassistant/turn_off', entity_id = 'light.entryway_light')
+                self.call_service('homeassistant/turn_off',
+                                  entity_id='light.entryway_light')
             if (data['command'] == 'rotate_right'):
-                self.call_service('homeassistant/turn_on', entity_id = 'light.entryway_light', brightness = 255)
-
+                self.call_service(
+                    'homeassistant/turn_on', entity_id='light.entryway_light', brightness=255)
