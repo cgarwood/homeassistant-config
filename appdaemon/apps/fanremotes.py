730ef6a60bb265cfc51364b13a5818000cb15e19
Charles Garwood <cgarwood@gmail.com>
2019-01-01 09:42:54 -0500

    Bedroom Pico Remote refactor & improvements

--- appdaemon/apps/fanremotes.py
+++ appdaemon/apps/fanremotes.py
@@ -4,70 +4,121 @@
 
 import appdaemon.plugins.hass.hassapi as hass
 
+
 class FanRemotes(hass.Hass):
 
     def initialize(self):
         self.listen_state(self.handle_light, "sensor.office_light_pico")
-        self.listen_state(self.handle_light, "sensor.master_bedroom_light_pico")
+        self.listen_state(self.handle_light,
+                          "sensor.master_bedroom_light_pico")
         self.listen_state(self.handle_light, "sensor.nursery_light_pico")
         self.listen_state(self.handle_fan, "sensor.office_fan_pico")
         self.listen_state(self.handle_fan, "sensor.master_bedroom_fan_pico")
         self.listen_state(self.handle_fan, "sensor.nursery_fan_pico")
-        
+
     def handle_light(self, entity, attribute, old, new, kwargs):
-        if (entity == "sensor.office_light_pico"):
-            controlled_entity = "light.guest_bedroom"
-        if (entity == "sensor.master_bedroom_light_pico"):
-            controlled_entity = "light.master_bedroom"
-        if (entity == "sensor.nursery_light_pico"):
-            controlled_entity = "light.nursery"
-            
+        remotes = {
+            "sensor.office_light_pico": {
+                'controlled_entity': 'light.guest_bedroom',
+                'favorite_level': 10
+            },
+            "sensor.master_bedroom_light_pico": {
+                'controlled_entity': 'light.master_bedroom',
+                'favorite_level': 10,
+                'tablet': 'master_bedroom'
+            },
+            "sensor.nursery_light_pico": {
+                'controlled_entity': 'light.nursery',
+                'favorite_level': 100,
+                'tablet': 'nursery'
+            },
+        }
+
+        remote = remotes[entity]
+
         if (new == '1'):
             # On
-            self.call_service('homeassistant/turn_on', entity_id = controlled_entity, brightness = 255)
-            
+
+            # Wake up TileBoard tablet if one is specified and the light was previously off
+            if ('tablet' in remote.keys()):
+                if (self.get_state(remote['controlled_entity']) == 'off'):
+                    self.fire_event(
+                        'tileboard', command='screen_on', target=remote['tablet'])
+
+            # Turn light on (full)
+            self.call_service('homeassistant/turn_on',
+                              entity_id=remote['controlled_entity'], brightness=255)
+
         if (new == '2'):
             # Favorite
-            self.call_service('homeassistant/turn_on', entity_id = controlled_entity, brightness = 25)
-            
+            self.call_service('homeassistant/turn_on',
+                              entity_id=remote['controlled_entity'], brightness=remote['favorite_level'])
+
         if (new == '4'):
             # Off
-            self.call_service('homeassistant/turn_off', entity_id = controlled_entity)
-            
+            self.call_service('homeassistant/turn_off',
+                              entity_id=remote['controlled_entity'])
+
         if (new == '8'):
             # Up
-            pass
-            
+            previous_level = self.get_state(
+                remote['controlled_entity'], attribute='brightness')
+            if not previous_level:
+                previous_level = 0
+
+            new_level = previous_level + 26
+            if (new_level > 255):
+                new_level = 255
+
+            self.call_service('homeassistant/turn_on',
+                              entity_id=remote['controlled_entity'],
+                              brightness=new_level)
+
         if (new == '16'):
             # Down
-            pass
-            
+            previous_level = self.get_state(
+                remote['controlled_entity'], attribute='brightness')
+            if not previous_level:
+                previous_level = 0
+
+            new_level = previous_level - 26
+            if (new_level < 1):
+                self.call_service('homeassistant/turn_off',
+                                  entity_id=remote['controlled_entity'])
+            else:
+                self.call_service('homeassistant/turn_on',
+                                  entity_id=remote['controlled_entity'],
+                                  brightness=new_level)
+
     def handle_fan(self, entity, attribute, old, new, kwargs):
-        
+
         if (entity == "sensor.office_fan_pico"):
             controlled_entity = "fan.guest_bedroom"
         if (entity == "sensor.master_bedroom_fan_pico"):
             controlled_entity = "fan.master_bedroom_fan"
         if (entity == "sensor.nursery_fan_pico"):
             controlled_entity = "fan.nursery_fan"
-            
+
         if (new == '1'):
             # On
-            self.call_service('fan/turn_on', entity_id = controlled_entity)
-            
+            self.call_service('fan/turn_on', entity_id=controlled_entity)
+
         if (new == '2'):
             # Favorite
-            self.call_service('fan/set_speed', entity_id = controlled_entity, speed = 'low')
-            
+            self.call_service(
+                'fan/set_speed', entity_id=controlled_entity, speed='low')
+
         if (new == '4'):
             # Off
-            self.call_service('homeassistant/turn_off', entity_id = controlled_entity)
-            
+            self.call_service('homeassistant/turn_off',
+                              entity_id=controlled_entity)
+
         if (new == '8'):
             # Up
-            self.call_service('fan/set_speed', entity_id = controlled_entity, speed = 'medium')
-            
+            self.call_service(
+                'fan/set_speed', entity_id=controlled_entity, speed='medium')
+
         if (new == '16'):
             # Down
-            self.call_service('fan/set_speed', entity_id = controlled_entity, speed = 'lowest')
-            
+            self.call_service(
+                'fan/set_speed', entity_id=controlled_entity, speed='lowest')
